4afd016264ecdd038ac8e48f0561791dd4ce33f0
Wayne <dev.wayne.ix@gmail.com>
2015-05-22 15:53:19 +0800

    update

--- index.js
+++ index.js
@@ -2,14 +2,22 @@ var through = require('through2');
 var gutil = require('gulp-util');
 
 var PluginError = gutil.PluginError;
-var appManifest =require('app-manifest');
+var appManifest = require('app-manifest');
 
 
 function gulpH5Manifest() {
 
     var stream = through.obj(function(file, enc, cb) {
-        file.contents = new Buffer(appManifest());
+        if (file.isNull()) {
+            // do nothing if no contents
+            var file = new gutil.File({
+                cwd: __dirname
+	    });
+        }
 
+        if (file.isBuffer()) {
+            file.contents = new Buffer(appManifest());
+        }
         this.push(file);
 
         return cb();
