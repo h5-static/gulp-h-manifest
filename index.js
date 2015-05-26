280154fcbd2f0df929f24d6dbcf210860876ea4a
yyy <super.doter.yyy@gmail.com>
2015-05-26 20:37:25 +0800

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var through = require('through2');
 var gutil = require('gulp-util');
 
 var PluginError = gutil.PluginError;
-var appManifest = require('manifest-generator');
+var appManifest = require('h5-manifest');
 
 
 function gulpH5Manifest(file) {
